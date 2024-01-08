1cf47efad0e9031dd657a8eb4ef61e90a7bcb627
danilya12 <72504166+danilya12@users.noreply.github.com>
2024-01-09 00:43:48 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public string sym;
+
+        public void SetX(int x)
+        {
+            if (x >= 0)
+                this.x = x;
+            else
+                throw new Exception("Значение x не может быть отрицательным");
+        }
+        public void SetY(int y)
+        {
+            if (y >= 0)
+                this.y = y;
+            else
+                throw new Exception("Значение y не может быть отрицательным");
+        }
+
+        public void SetSym(string symbol)
+        {
+            sym = symbol;
+        }
+        public void Draw(string sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(sym);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Point a = new Point
+
+            {
+                x = 1,
+                y = 2,
+            };
+
+            a.Draw("----");
+            Point b = new Point
+            {
+                x = 1 + 1,
+                y = 2 + 1,
+            };
+            b.Draw("##--");
+            Point c = new Point
+            {
+                x = 1,
+                y = 2 + 2,
+            };
+            c.Draw("----");
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Point
+    {
+        int x;
+        int y;
+        char sym;
+
+        public Point(int x, int y, char sym)
+        {
+            this.x = x;
+            this.y = y;
+            this.sym = sym;
+        }
+
+        public void SetX(int x)
+        {
+            if (x >= 0)
+                this.x = x;
+            else
+                throw new Exception("Значение x не может быть отрицательным");
+        }
+        public void SetY(int y)
+        {
+            if (y >= 0)
+                this.y = y;
+            else
+                throw new Exception("Значение y не может быть отрицательным");
+        }
+        public void SetSym(char symbol)
+        {
+            sym = symbol;
+        }
+        public void Draw()
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
+        }
+    }
+    class Figure
+    {
+        protected List<Point> pList;
+
+        public void Draw()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+    class VerticalLine : Figure
+    {
+        public VerticalLine(int yUp, int yDown, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yUp; y <= yDown; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+
+    class HorizontalLine : Figure
+    {
+        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HorizontalLine h1 = new HorizontalLine(2, 7, 4, '*');
+            h1.Draw();
+            HorizontalLine h2 = new HorizontalLine(2, 7, 7, '*');
+            h2.Draw();
+            VerticalLine v1 = new VerticalLine(4, 7, 2, '*');
+            v1.Draw();
+            VerticalLine v2 = new VerticalLine(4, 7, 7, '*');
+            v2.Draw(); 
+            
+            Console.ReadKey();
+        }
+    }
+}
